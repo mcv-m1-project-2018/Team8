54ff05616dd8408c19c1c65c8fb2130ffa5ae3c2
richardseba <richard.segovia.b@outlook.com>
2018-10-06 11:11:01 +0200

    filters added to functions

--- traffic_signs/segmentation.py
+++ traffic_signs/segmentation.py
@@ -10,8 +10,13 @@ import numpy as np
 def whitePatch(im):
 	bmax, gmax, rmax = np.amax(np.amax(im,axis=0),axis=0)
 
-	print(bmax,gmax,rmax)
+	alpha = gmax/rmax
+	beta = gmax/bmax
 
+	im[:,:,2] = alpha*im[:,:,2]
+	im[:,:,0] = beta*im[:,:,0]
+
+	return im
 
 def grayWorld(im):
 	bmean, gmean, rmean = np.mean(np.mean(im,axis=0),axis=0)
@@ -24,38 +29,42 @@ def grayWorld(im):
 
 	return im
 
+def handPickedMaskFilters(im):
+	#red colored signals
+	mskr = image[:,:,2] > 70
+	mskr = mskr*(image[:,:,1] < 50)
+	mskr = mskr*(image[:,:,0] < 50)
+	mskr = np.dstack([mskr]*3)
+
+	#blue colored signals
+	mskb = image[:,:,2] < 50
+	mskb = mskb*(image[:,:,1] < 100)
+	mskb = mskb*(image[:,:,0] > 60)
+	mskb = np.dstack([mskb]*3)
+
+	msk = mskr + mskb
+
+	return msk
+
+
 def segmentate(im_directory):
 	file_names = sorted(fnmatch.filter(os.listdir(im_directory), '*.jpg'))
-
 
 	#For each file 
 	for name in file_names:
 		base, extension = os.path.splitext(name)
-
 		
 		imageNameFile = im_directory + "/" + name
 		print(imageNameFile)
 		image = cv.imread(imageNameFile)
 		image = grayWorld(image)
 
-		
-		#red filter
-		mskr = image[:,:,2] > 70
-		mskr = mskr*(image[:,:,1] < 50)
-		mskr = mskr*(image[:,:,0] < 50)
-		mskr = np.dstack([mskr]*3)
-
-		mskb = image[:,:,2] < 50
-		mskb = mskb*(image[:,:,1] < 100)
-		mskb = mskb*(image[:,:,0] > 60)
-		mskb = np.dstack([mskb]*3)
-		msk = mskr + mskb
-
+		msk = handPickedMaskFilters(image)
 
 		img = image*msk
 
-		cv.imshow("asd",img)
-		cv.imshow("das",image)
+		cv.imshow("masked image",img)
+		cv.imshow("original image",image)
 		cv.waitKey(0)
 
 
