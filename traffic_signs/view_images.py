797c229bb7e8e846213f736c4ed00a104cd3f680
richardseba <richard.segovia.b@outlook.com>
2018-10-14 19:49:40 +0200

    bounding boxes

--- traffic_signs/view_images.py
+++ traffic_signs/view_images.py
@@ -1,7 +1,41 @@
 import cv2
 import os
+from PIL import Image, ImageDraw
 from main import CONSOLE_ARGUMENTS
+import matplotlib.pyplot as plt
 import numpy as np
+
+
+def fill_holes(im):
+    im_th = cv2.cvtColor(im.copy(), cv2.COLOR_RGB2GRAY)
+
+    _, contours, _ = cv2.findContours(im_th,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+    for cnt in contours:
+        cv2.drawContours(im_th,[cnt],0,255,-1)
+    
+    return im_th
+
+def detectBoundingBoxes(im):
+    _, contours, _ = cv2.findContours(im,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    bb_list = list()
+    for cnt in contours:
+        x,y,w,h = cv2.boundingRect(cnt)   
+        bb_list.append((x,y,w,h))
+
+
+    return bb_list
+
+def boundingBoxFilter(bb_list, im):
+    image = im.copy()
+    for x,y,w,h in bb_list:
+        if(w < 25 and h < 25):
+            image[y:y+h, x:x+w] = np.zeros((h,w))
+
+    return image
+
+
+    
 
 directory = CONSOLE_ARGUMENTS.im_directory
 dirs = os.listdir(directory)
@@ -28,10 +62,17 @@ for im_path in dirs:
     # opening = cv2.erode(opening,bigerer_kernel,iterations=1)
     # opening = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, biger_kernel)
     # opening = cv2.dilate(opening,bigerest_kernel,iterations=1)
+    imagen = fill_holes(opening)
+
+    bb_list = detectBoundingBoxes(imagen)
+    imagen = boundingBoxFilter(bb_list,imagen)
 
     small_op = cv2.resize(opening, (0,0), fx=0.5, fy=0.5) 
+    small_fill = cv2.resize(imagen, (0,0), fx=0.5, fy=0.5) 
     small_im = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
+
     cv2.imshow('mod',small_op*255)
+    cv2.imshow('fill',small_fill)
     cv2.imshow('original', small_im*255)
     k = cv2.waitKey()
     if k==27: # Esc key to stop
