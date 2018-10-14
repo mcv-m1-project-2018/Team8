18aa535333b01e1ca5ecc69ac8211799f6b8c80f
Andreu Rodríguez <43759949+AndreuRodriguez@users.noreply.github.com>
2018-10-14 11:29:28 +0200

    Update morph_operators.py

--- traffic_signs/morph_operators.py
+++ traffic_signs/morph_operators.py
@@ -1,20 +1,24 @@
+'''This code is only a reference code from which we can select the
+desired kernel structure and size from the square, rectangular, ellipsoidal
+and cross options. It also has an example of erosion, dilation, opening and
+closing.'''
 import cv2
 import numpy as np
 
-#Defining the kernel
+#Defining the kernel shape and size using CV.
 square_kernel = np.ones((5,5), iterations=1) #This is a 5X5 kernel.
 rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
 ellip_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE(5,5))
 cross_kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(5,5))
 
-#Erosion filter
+#Erosion filter with a square kernel.
 erosion = cv2.erode(image,square_kernel,iterations=1)
 
-#Dilation filter
+#Dilation filter with a square kernel.
 dilation = cv2.dilate(image, square_kernel, iterations=1)
 
-#Opening
+#Opening filter with a square kernel.
 opening = cv2.morphologyEx(image, cv2.MORPH_OPEN, square_kernel)
 
-#Closing
+#Closing filter with a square kernel.
 closing = cv2.morphologyEx(image, cv2.MORPH_CLOSE, square_kernel)
