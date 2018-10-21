4eaa9374dc08be22ae2cb6d0ce0686a5e8b6bb39
nilaisr <nilaisr@gmail.com>
2018-10-21 12:28:22 +0200

    anged variable names in morphology

--- traffic_signs/morphology.py
+++ traffic_signs/morphology.py
@@ -8,40 +8,31 @@ def fill_holes(im):
         cv.drawContours(im,[cnt],0,255,-1)
     return im
 
-biger_kernel = np.ones((8,8), np.uint8)
-bigerer_kernel = np.ones((14,14), np.uint8)
-bigerest_kernel = np.ones((20,20), np.uint8)
-
-# norm_kernel = np.ones((4,4), np.uint8)
 small_kernel = np.ones((3,3), np.uint8)
-# smaller_kernel = np.ones((2,2), np.uint8)
-# smallest_kernel = np.ones((1,1), np.uint8)
+medium_kernel = np.ones((8,8), np.uint8)
+big_kernel = np.ones((14,14), np.uint8)
+huge_kernel = np.ones((20,20), np.uint8)
+
 h_kernel = np.ones((1,4), np.uint8)
 v_kernel = np.ones((4,1), np.uint8)
     
-def morf_method1(im):
+def morph_method1(im):
     imggray = im.copy()
-    # print(im.shape)
-    # imggray = cv.cvtColor(im.copy(), cv.COLOR_RGB2GRAY)
-    opening = imggray.copy()
-    opening = cv.erode(opening,(2,2),iterations=1)
-#     opening = cv.morphologyEx(opening, cv.MORPH_OPEN, small_kernel)
-    # big_kernel = np.ones((6,6), np.uint8)
-    
 
-    opening = cv.morphologyEx(opening, cv.MORPH_CLOSE, bigerest_kernel)
-    opening = fill_holes(opening)
-    opening = cv.morphologyEx(opening, cv.MORPH_OPEN, small_kernel)
-    opening = cv.morphologyEx(opening, cv.MORPH_OPEN, h_kernel)
-    opening = cv.morphologyEx(opening, cv.MORPH_OPEN, v_kernel)
-    opening = cv.morphologyEx(opening, cv.MORPH_CLOSE, bigerer_kernel)
-    opening = cv.morphologyEx(opening, cv.MORPH_CLOSE, bigerest_kernel)
-    opening = fill_holes(opening)
-    opening = cv.morphologyEx(opening, cv.MORPH_OPEN, biger_kernel)
-    # opening = cv.erode(opening,bigerer_kernel,iterations=1)
-    # opening = cv.morphologyEx(opening, cv.MORPH_CLOSE, bigerest_kernel)
-    # opening = cv.dilate(opening,bigerest_kernel,iterations=1)
-    imagen = fill_holes(opening)
+    morph = imggray.copy()
+    morph = cv.erode(morph,(2,2),iterations=1)
+  
+    morph = cv.morphologyEx(morph, cv.MORPH_CLOSE, huge_kernel)
+    morph = fill_holes(morph)
+    morph = cv.morphologyEx(morph, cv.MORPH_OPEN, small_kernel)
+    morph = cv.morphologyEx(morph, cv.MORPH_OPEN, h_kernel)
+    morph = cv.morphologyEx(morph, cv.MORPH_OPEN, v_kernel)
+    morph = cv.morphologyEx(morph, cv.MORPH_CLOSE, big_kernel)
+    morph = cv.morphologyEx(morph, cv.MORPH_CLOSE, huge_kernel)
+    morph = fill_holes(morph)
+    morph = cv.morphologyEx(morph, cv.MORPH_OPEN, medium_kernel)
+
+    imagen = fill_holes(morph)
 
     rows,cols= imagen.shape
     M = np.float32([[1,0,-5],[0,1,-5]])
@@ -49,65 +40,49 @@ def morf_method1(im):
 
     return imagen
 
-def morf_method2(im):
+def morph_method2(im):
     imggray = im.copy()
-    opening = imggray.copy()
-    opening = cv.erode(opening,(2,2),iterations=1)
-    opening = cv.morphologyEx(opening, cv.MORPH_CLOSE, bigerest_kernel)
-    opening = fill_holes(opening)
+    morph = imggray.copy()
+    morph = cv.erode(morph,(2,2),iterations=1)
+    morph = cv.morphologyEx(morph, cv.MORPH_CLOSE, huge_kernel)
+    morph = fill_holes(morph)
 
-#    rows,cols= imagen.shape
-#    M = np.float32([[1,0,-5],[0,1,-5]])
-#    imagen = cv.warpAffine(imagen,M,(cols,rows))
+    return morph
 
-    return opening
-
-def morf_method3(im):
+def morph_method3(im):
     imggray = im.copy()
-    opening = imggray.copy()
-    opening = cv.erode(opening,(2,2),iterations=1)
+    morph = imggray.copy()
+    morph = cv.erode(morph,(2,2),iterations=1)
 
-    
+    morph = cv.morphologyEx(morph, cv.MORPH_CLOSE, big_kernel)
+    morph = fill_holes(morph)
+    morph = cv.morphologyEx(morph, cv.MORPH_OPEN, small_kernel)
+    morph = cv.morphologyEx(morph, cv.MORPH_CLOSE, big_kernel)
+    morph = fill_holes(morph)
 
-    opening = cv.morphologyEx(opening, cv.MORPH_CLOSE, bigerer_kernel)
-    opening = fill_holes(opening)
-    opening = cv.morphologyEx(opening, cv.MORPH_OPEN, small_kernel)
-    opening = cv.morphologyEx(opening, cv.MORPH_CLOSE, bigerer_kernel)
-    opening = fill_holes(opening)
+    return morph
 
-#    rows,cols= imagen.shape
-#    M = np.float32([[1,0,-5],[0,1,-5]])
-#    imagen = cv.warpAffine(imagen,M,(cols,rows))
-
-    return opening
-
-def morf_method4(im):
+def morph_method4(im):
     imggray = im.copy()
-    opening = imggray.copy()
-    opening = cv.erode(opening,(2,2),iterations=1)
+    morph = imggray.copy()
+    morph = cv.erode(morph,(2,2),iterations=1)
 
-    
+    morph = cv.morphologyEx(morph, cv.MORPH_CLOSE, huge_kernel)
+    morph = fill_holes(morph)
+    morph = cv.morphologyEx(morph, cv.MORPH_OPEN, small_kernel)
+    morph = cv.morphologyEx(morph, cv.MORPH_CLOSE, huge_kernel)
+    morph = fill_holes(morph)
 
-    opening = cv.morphologyEx(opening, cv.MORPH_CLOSE, bigerest_kernel)
-    opening = fill_holes(opening)
-    opening = cv.morphologyEx(opening, cv.MORPH_OPEN, small_kernel)
-    opening = cv.morphologyEx(opening, cv.MORPH_CLOSE, bigerest_kernel)
-    opening = fill_holes(opening)
-
-#    rows,cols= imagen.shape
-#    M = np.float32([[1,0,-5],[0,1,-5]])
-#    imagen = cv.warpAffine(imagen,M,(cols,rows))
-
-    return opening
+    return morph
 
 
 
 def apply_morphology(im, morphology):
     switcher_morf = {
-        'm1': morf_method1,
-        'm2': morf_method2,
-        'm3': morf_method3,
-        'm4': morf_method4
+        'm1': morph_method1,
+        'm2': morph_method2,
+        'm3': morph_method3,
+        'm4': morph_method4
     }
     
     # PIXEL MORPHOLOGY
