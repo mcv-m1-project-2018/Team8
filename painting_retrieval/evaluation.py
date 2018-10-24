9e56b80bb938c6fae953d35a20a2f86dd60b7658
AndreuRodriguez <andreurodriguezorihuela@gmail.com>
2018-10-24 18:58:54 +0200

    Evaluation with similarity measures

--- painting_retrieval/evaluation.py
+++ painting_retrieval/evaluation.py
@@ -1,3 +1,21 @@
 import numpy as np
 import cv2 as cv
 import ml_metrics as metrics
+import numpy as np
+
+def euclidean_distance(t_bins, q_bins):
+    for j in range(len(t_bins)):
+        euclidean_dist = sqrt((t_bins(1) - q_bins(1)) ^ 2 + (t_bins(2) - q_bins(2)) ^ 2 +
+                              (t_bins(3) - q_bins(3)) ^ 2)
+        return euclidean_dist
+
+def l_1_distance():
+
+
+def x_sq():
+
+
+def hist_intersect():
+
+
+def kernhell():
