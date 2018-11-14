cc35a8c5a3fac5be07f63cd84b708e2fd9b58c82
Andreu Rodríguez <43759949+AndreuRodriguez@users.noreply.github.com>
2018-11-14 20:31:51 +0100

    Update crop_and_rotate.py

--- painting_retrieval/preprocess/crop_and_rotate.py
+++ painting_retrieval/preprocess/crop_and_rotate.py
@@ -1,11 +1,11 @@
 import cv2 as cv
 from tqdm import tqdm
 import numpy as np
-import imutils 
+import imutils
 from math import degrees
 
 
-def rotate(file_names, image_path):
+def compute_angle(file_names, image_path):
     n_images = len(file_names)
     
     i = 0
@@ -14,19 +14,19 @@ def rotate(file_names, image_path):
         imageNameFile = image_path + "/" + name
         image = cv.imread(imageNameFile)
 
-        gray = cv.cvtColor(image,cv.COLOR_BGR2GRAY)
-        k = np.ones((9,9))
-        gray = cv.blur(gray,(9,9))
-        edges = cv.Canny(gray,10,20)
-        edges2 = cv.resize(edges,None, fx=0.3, fy=0.3)
-        cv.imshow('e',edges2)
+        gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        k = np.ones((9, 9))
+        gray = cv.blur(gray, (9, 9))
+        edges = cv.Canny(gray, 10, 20)
+        edges2 = cv.resize(edges, None, fx=0.3, fy=0.3)
+        cv.imshow('e', edges2)
 
 
         meanLines = []
 
-        lines = cv.HoughLines(edges,1,np.pi/180,100)
+        lines = cv.HoughLines(edges, 1, np.pi/180, 100)
         for line in lines:
-            for rho,theta in line:
+            for rho, theta in line:
                 if meanLines != []:
                     foundMean = False
                     for eachMean in meanLines:
@@ -39,36 +39,35 @@ def rotate(file_names, image_path):
                         if foundMean:
                             break
                     if not foundMean:
-                        meanLines.append([rho, rho, theta , 1])          
+                        meanLines.append([rho, rho, theta, 1])
                          
                 else:
                     meanLines.append([rho, rho, theta, 1])
-
         print(meanLines)
-        rotate_rho1(meanLines,image)
-        
-        image2 = cv.resize(image,None, fx=0.1, fy=0.1)
-        cv.imshow('lines',image2)
+
+        rotate(meanLines, image)
+
+        image2 = cv.resize(image, None, fx=0.1, fy=0.1)
+        cv.imshow('lines', image2)
         k = cv.waitKey()
-        
-        if k==27 or k==-1:    # Esc key or close to stop
+
+        if k == 27 or k == -1:    # Esc key or close to stop
             break
-        elif k==97 and i>0:    # A to go back
-            i-=1
+        elif k == 97 and i > 0:    # A to go back
+            i -= 1
         else:                   # Any key to go forward
-            i+=1
+            i += 1
 
 
-def rotate_rho1(meanLines, image):
-    for (rho1, rho2, theta , _) in meanLines:
-        rot_im_rho1 = imutils.rotate_bound(image, 360-degrees(rho1))
-        rot_im_rho2 = imutils.rotate_bound(image, 360-degrees(rho2))
-        rot_im_theta = imutils.rotate_bound(image, 360-degrees(theta))
-
-        rot_im_rho1 = cv.resize(rot_im_rho1,None, fx=0.1, fy=0.1)
-        rot_im_rho2 = cv.resize(rot_im_rho2,None, fx=0.1, fy=0.1)
-        rot_im_theta = cv.resize(rot_im_theta,None, fx=0.1, fy=0.1)
-        cv.imshow('Rotated without losing rho1', rot_im_rho1)
-        cv.imshow('Rotated without losing rho2', rot_im_rho2)
-        cv.imshow('Rotated without losing theta', rot_im_theta)
+def rotate(meanLines, image):
+    for (rho1, rho2, theta, _) in meanLines:
+        rot_rho1 = imutils.rotate_bound(image, 360-degrees(rho1))
+        res_rot_rho1 = cv.resize(rot_rho1, None, fx=0.1, fy=0.1)
+        rot_rho2 = imutils.rotate_bound(image, 360-degrees(rho2))
+        res_rot_rho2 = cv.resize(rot_rho2, None, fx=0.1, fy=0.1)
+        rot_theta = imutils.rotate_bound(image, 360-degrees(theta))
+        res_rot_theta = cv.resize(rot_theta, None, fx=0.1, fy=0.1)
+        cv.imshow('Rotated without losing rho1', res_rot_rho1)
+        cv.imshow('Rotated without losing rho2', res_rot_rho2)
+        cv.imshow('Rotated without losing theta', res_rot_theta)
         cv.waitKey(0)
