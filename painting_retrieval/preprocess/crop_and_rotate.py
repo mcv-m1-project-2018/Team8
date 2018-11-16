rotation corrected

painting_retrieval/preprocess/crop_and_rotate.py
@@ -2,9 +2,16 @@ import cv2 as cv
 from tqdm import tqdm
 import numpy as np
 import imutils
-from math import degrees
+from math import degrees, radians
 from skimage import feature
 
+
+def resize_keeping_ar(im, desired_width=300):
+    height, width = im.shape[:2]
+    factor = width/float(desired_width)
+    desired_height = int(height/factor)
+    imres = cv.resize(im, (desired_width, desired_height))
+    return imres
 
 def compute_angles(file_names, image_path):
     n_images = len(file_names)
@@ -14,20 +21,21 @@ def compute_angles(file_names, image_path):
         name = file_names[i]
         imageNameFile = image_path + "/" + name
         image = cv.imread(imageNameFile)
-
+        w,h = image.shape[:2]
+        new_size = min(round(w*0.2), 300)
+        image = resize_keeping_ar(image, )
         gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
         k = np.ones((9, 9))
         print(type(gray))
-        gray = cv.blur(gray, (9, 9))
-        edges = cv.Canny(gray, 90, 130, apertureSize=3)
-        # edges = feature.canny(gray)
-        edges2 = cv.resize(edges, None, fx=0.3, fy=0.3)
+        gray = cv.GaussianBlur(gray,(9,9),0)
+        edges = cv.Canny(gray, 100, 130, apertureSize=3, L2gradient=True)
+        edges2 = resize_keeping_ar(edges)
         cv.imshow('e', edges2)
 
 
         meanLines = []
 
-        lines = cv.HoughLines(edges, 1, np.pi/180, 100)
+        lines = cv.HoughLines(edges, 1, np.pi/180, 50)
         for line in lines:
             for rho, theta in line:
                 if meanLines != []:
@@ -46,11 +54,10 @@ def compute_angles(file_names, image_path):
                          
                 else:
                     meanLines.append([rho, rho, theta, 1])
-        # print(meanLines)
 
         rotate(meanLines, image)
 
-        image2 = cv.resize(image, None, fx=0.1, fy=0.1)
+        image2 = resize_keeping_ar(image)
         cv.imshow('lines', image2)
         k = cv.waitKey()
 
@@ -62,26 +69,27 @@ def compute_angles(file_names, image_path):
             i += 1
 
 
-def rotate(meanLines, image):
+def rotate(meanLines, image, thr_angle=60, inc=5):
     # for values in meanLines:
     def thirdElement(elem):
         return elem[2]
 
-    # sortedLines = sorted(meanLines, key=thirdElement, reverse=True)
-    sortedLines = sorted(meanLines, key = lambda x: x[3], reverse=True)
-    values = sortedLines[0]
+    countLines = sum([x[3] for x in meanLines])
+    rot_filter_lines = [x for x in meanLines if (degrees(x[2])<thr_angle or degrees(x[2])>(360-thr_angle))]
+    while rot_filter_lines == []:
+        rot_filter_lines = [x for x in meanLines if (degrees(x[2])<(thr_angle+inc) or degrees(x[2])>(360-thr_angle+inc))]
+        inc += 5
+
+    if inc > 15:
+        rot_filter_lines = [(lambda x: [x[0],x[1],x[2]-radians(90),x[3]])(x) for x in rot_filter_lines]
+    # thr_lines = [x for x in rot_filter_lines if x[3]>(countLines/4)]
+    thr_lines = sorted(rot_filter_lines, key = lambda x: x[2], reverse=True)
+    values = thr_lines[0]
     theta = values[2]
 
-    print(sortedLines)
+    # print(sortedLines)
 
     rot_theta = imutils.rotate_bound(image, 360-degrees(theta))
-    res_rot_theta = cv.resize(rot_theta, None, fx=0.1, fy=0.1)
+    res_rot_theta = resize_keeping_ar(rot_theta)
     cv.imshow('Rotated theta', res_rot_theta)
-    cv.waitKey(0)
-
-    # rot_rho1 = imutils.rotate_bound(image, 360-degrees(rho1))
-    # res_rot_rho1 = cv.resize(rot_rho1, None, fx=0.1, fy=0.1)
-    # rot_rho2 = imutils.rotate_bound(image, 360-degrees(rho2))
-    # res_rot_rho2 = cv.resize(rot_rho2, None, fx=0.1, fy=0.1)
-    # cv.imshow('Rotated without losing rho1', res_rot_rho1)
-    # cv.imshow('Rotated without losing rho2', res_rot_rho2)
+    # cv.waitKey(0)